e67f48433aeb55082ce6a1498002a5e9ef743c5f
SuichuKodama <kodama@suichu.net>
2023-07-29 02:43:27 +0900

    css修正

--- project/resources/views/tweet/update.blade.php
+++ project/resources/views/tweet/update.blade.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Fly Tweet</title>
+  @include('_head')
+  <link rel="stylesheet" href="/css/style.css">
+  <title>編集 | Fly Tweet</title>
 </head>
 <body>
-  <h1>Fly Tweet</h1>
-  <div>
-    <a href="{{ route('tweet.index') }}">戻る</a>
-    <p>投稿フォーム</p>
-    @if (session('feedback.success'))
-      <p>{{ session('feedback.success') }}</p>
-    @endif
-    <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
-      @method('put')
-      @csrf
-      <label for="tweet-content">つぶやき</label>
-      <span>140文字まで</span>
-      <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力">{{ $tweet->content }}</textarea>
-      @error('tweet')
-      <p>{{ $message }}</p>
-      @enderror
-      <button type="submit">編集</button>
-    </form>   
+  <h1 class="heading-title">Fly Tweet</h1>
+  <div class="main-container">
+    <div class="update-container">
+      <div class="mountain-bg"></div>
+      @if (session('feedback.success'))
+        <p>{{ session('feedback.success') }}</p>
+      @endif
+      <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
+        @method('put')
+        @csrf
+        <div class="btn-list">
+          <label for="tweet-content">つぶやきを編集</label>
+          <button class="btn update" type="submit">編集</button>
+        </div>
+        <textarea class="input textarea-update" name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力">{{ $tweet->content }}</textarea>
+        @error('tweet')
+        <p>{{ $message }}</p>
+        @enderror
+      </form>
+      <a class="btn back" href="{{ route('tweet.index') }}">戻る</a> 
+    </div>
   </div>
 </body>
 </html>
